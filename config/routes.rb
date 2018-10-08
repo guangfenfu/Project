132fa8586d5fd54ff4fb949fd9c076671b6caf36
Faye Fu <guangfenfu@gmail.com>
2018-10-08 14:09:57 -0500

    create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'stocks/index'
+  get 'products/index'
+  get 'stores/index'
+  get 'colors/index'
+  get 'currencies/index'
+  get 'categories/index'
+  get 'brands/index'
+  get 'taxes/index'
+  get 'provinces/index'
+  get 'addresses/index'
+  get 'roles/index'
+  get 'users/index'
+  get 'user/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

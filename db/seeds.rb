8d9c0133c57b7fc400dcd8cb0ad44fe1e84b4f44
Faye Fu <guangfenfu@gmail.com>
2018-10-11 22:43:02 -0500

    pull data from Role, User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User.destroy_all
+# Role.destroy_all
+# Address.destroy_all
+# Province.destroy_all
+# Tax.destroy_all
+# Brand.destroy_all
+# Category.destroy_all
+# Product.destroy_all
+# ProductColor.destroy_all
+# Currency.destroy_all
+# Stock.destroy_all
+# Store.destroy_all
+# Color.destroy_all
+
+
+role = Role.create(:name => "admin", :description => "Administrator")
+role.users.create(:name =>Faker::Name.first_name, :password => Faker::Code.ean)
+
+role = Role.create(:name => "customer", :description => "Normal customer")
+10.times {
+  role.users.create(:name =>Faker::Name.first_name, :password => Faker::Code.ean)
+}
+
+
+
+
